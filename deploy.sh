b26dfc0fc41c13cc8be11623900389b240f42f16
tehj <julian.teh@autodesk.com>
2019-12-20 14:27:00 +0800

    Updating deploy script a bit

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo Deploying from $DIR
 
 echo Installing all necessary packages
+# Update and install the sub modules
 cd ../BotSecrets
 git pull
 go install
 cd ../ScriptureBot
 git pull
 go install
+
+echo Installing app entry package
+# Update and install the entry module
 cd $DIR
 git pull
 go install
