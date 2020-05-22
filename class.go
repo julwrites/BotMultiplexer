2f32e896eeca06363a71a9bc0a63e04ba34ab48d
julwrites <julian.teh@outlook.com>
2020-05-22 23:23:40 +0800

    Updating definition of user data to be generic

--- class.go
+++ class.go
@@ -4,19 +4,13 @@ import botsecrets "github.com/julwrites/BotSecrets"
 
 // Struct definitions for bot
 
-type UserConfig struct {
-	Version       string `datastore:""`
-	Timezone      string `datastore:""`
-	Subscriptions string `datastore:""`
-}
-
 type UserData struct {
-	Firstname string     `datastore:""`
-	Lastname  string     `datastore:""`
-	Username  string     `datastore:""`
-	Id        string     `datastore:""`
-	Type      string     `datastore:""`
-	Config    UserConfig `datastore:",flatten"`
+	Firstname string `datastore:""`
+	Lastname  string `datastore:""`
+	Username  string `datastore:""`
+	Id        string `datastore:""`
+	Type      string `datastore:""`
+	Config    string `datastore:""`
 }
 
 type MessageData struct {
