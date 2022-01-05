ca8e5a0b7afe1feca42f8331702fd407954cfe68
julwrites <julian.teh@outlook.com>
2022-01-05 17:39:41 +0800

    Fixing SecretsData return

--- pkg/secrets/secrets.go
+++ pkg/secrets/secrets.go
@@ -13,7 +13,7 @@ func LoadSecrets(filePath string) (SecretsData, error) {
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Printf("Error reading secrets: %v", err)
-		return nil, err
+		return SecretsData{}, err
 	}
 
 	// log.Printf("Raw Secrets: %s", strings.ReplaceAll(string(data), "\n", ""))
@@ -22,7 +22,7 @@ func LoadSecrets(filePath string) (SecretsData, error) {
 	err = yaml.Unmarshal([]byte(data), &env)
 	if err != nil {
 		log.Printf("Error unmarshaling secrets: %v", err)
-		return nil, err
+		return SecretsData{}, err
 	}
 
 	return env, nil
